de03f1825fc75f8f86b7ca422cd605c7230998a2
stephane0614 <nsimba.stephane033@gmail.com>
2022-11-27 19:39:41 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,52 @@
+// ************************************** compte a rebours ********************************************
+let timer = 10;
+let button;
+let timelaps;
+let billboard;
+let disable;
+let rocket;
+let buttonReset={
+    green:"style\images\firing-button.png",
+    red:"style\images\cancel-button.png"
+}
+let rocketStart = {
+    start: "C:\Users\pheni\CODE\Rocket\style\images\rocket2.gif",
+    fly: "C:\Users\pheni\CODE\Rocket\style\images\rocket3.gif",
+};
+
+
+const main = () => {
+    billboard = document.querySelector("#billboard span");
+    button = document.getElementById("firing-button");
+    rocket = document.getElementById("rocket");
+    button.addEventListener('click', disable);
+};
+
+disable = () => {
+    button.classList.add("disabled");
+    rocket.src = rocketStart.start;
+    timelaps();
+}
+
+timelaps = () => {
+    billboard.innerText = timer;
+    timer--;
+    if (timer >= 0) {
+        setTimeout(timelaps, 1000);
+    }
+    else {
+        clearTimeout(timelaps);
+        button.classList.remove("disabled");
+        button.src=buttonReset.red;
+        takeOff();
+    };
+};
+
+const takeOff = () => {
+    rocket.src = rocketStart.fly;
+    rocket.classList.add("tookOff");
+   
+};
+
+
+addEventListener('load', main);
